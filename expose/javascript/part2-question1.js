9e2584324eee12f535149cb1a62809ea2c55bd4c
jackmontoro <jmontoro@ucsd.edu>
2023-04-30 09:35:51 -0700

    answered pt2q1

--- expose/javascript/part2-question1.js
+++ expose/javascript/part2-question1.js
@@ -1,3 +1,4 @@
+// Question 1
 function discountPrices(prices, discount) {
     var discounted = [];
     var finalPrice = 0;
--- expose/javascript/part2.md
+++ expose/javascript/part2.md
@@ -1,1 +1,3 @@
+1. The code prints "3". This is true because var had different scope than lets and const.
 
+2.
